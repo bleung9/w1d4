5743ff222622c22183caf0a2b9a648e3a939f1b1
Bosco <bleung9@gmail.com>
2019-03-07 16:51:47 +0000

    finding waldo w/ forEach

--- finding-waldo.js
+++ finding-waldo.js
@@ -1,10 +1,22 @@
+// // The second argument/parameter is expected to be a function
+// function findWaldo(arr, found) {
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] === "Waldo") {
+//       found(i);   // execute callback
+//     }
+//   }
+// }
+
+// function actionWhenFound(index) {
+//   console.log("Found him at index", index);
+// }
+
+// findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
-    }
-  }
+  arr.forEach(function(element, index) { element === "Waldo" ? found(index) : 1 })
 }
 
 function actionWhenFound(index) {
